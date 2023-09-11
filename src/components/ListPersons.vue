de982855cc4e264bfd73d329535793cec9d5b5ef
evertonreis <evertonreisnascimento3@gmail.com>
2023-09-11 14:22:39 -0300

    Imagens da API de listagem

---
+++ src/components/ListPersons.vue
@@ -0,0 +1,20 @@
+<script setup>
+const person = defineProps(["name", "url"])
+
+</script>
+
+<template>
+    <div class="col-4">
+        <div class="card mb-1">
+            <img src="" class="card-img-top" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">{{ person.name }}</h5>
+                <a :href="url" class="btn btn-primary">Detalhes</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
